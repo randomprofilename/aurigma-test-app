9e17d74d691e0ba8e7c153efb1daf2d07422e3d7
randomprofilename <zeum73@gmail.com>
2019-08-29 22:05:29 +0700

    Simple routing

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { serverPort } = require("./config");
+const router = require("./routes");
 
 const app = express();
 
 app.use(express.json());
+app.use(router);
 
 app.listen(serverPort, () => console.log(`Listen port ${serverPort}`));
---
+++ backend/routes/api.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const { files } = require("../controllers/api");
+
+router.get("files", files.get);
+// router.post("files")
+
+module.exports = router;
