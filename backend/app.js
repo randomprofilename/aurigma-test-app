3202a3ecc8da5e5837a862966cd5f951ea0a1fc1
randomprofilename <zeum73@gmail.com>
2019-08-29 22:10:01 +0700

    morgan installed

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
 const { serverPort } = require("./config");
 const router = require("./routes");
 
 const app = express();
 
+app.use(morgan('common'))
 app.use(express.json());
 app.use(router);
 
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { files } = require("../controllers/api");
 
-router.get("files", files.get);
+router.get("/files", files.get);
 // router.post("files")
 
 module.exports = router;
